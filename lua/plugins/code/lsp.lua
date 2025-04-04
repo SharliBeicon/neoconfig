ae793aa7c4168cf876db9cd2574bf1cb6a42245b
Charlie Bacon <ct@databeacon.aero>
2025-04-04 23:18:40 +0200

    delete unnecessary lsp info

--- lua/plugins/code/lsp.lua
+++ lua/plugins/code/lsp.lua
@@ -22,9 +22,6 @@ return {
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
-
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
     },
--- lua/plugins/ui/ui.lua
+++ lua/plugins/ui/ui.lua
@@ -51,7 +51,6 @@ return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      dofile(vim.g.base46_cache .. 'git'),
       signs = {
         delete = { text = '󰍵' },
         changedelete = { text = '󱕖' },
