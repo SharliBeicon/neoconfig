90f208c2b75c5eaabbceced227655b2a5914fabc
Charlie Bacon <ct@databeacon.aero>
2025-04-04 14:12:26 +0200

    action for indentation

--- init.lua
+++ init.lua
@@ -82,6 +82,32 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.g.base46_cache = vim.fn.stdpath 'data' .. '/base46_cache/'
 
+-- user event that loads after UIEnter + only if file buf is there
+vim.api.nvim_create_autocmd({ 'UIEnter', 'BufReadPost', 'BufNewFile' }, {
+  group = vim.api.nvim_create_augroup('NvFilePost', { clear = true }),
+  callback = function(args)
+    local file = vim.api.nvim_buf_get_name(args.buf)
+    local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+
+    if not vim.g.ui_entered and args.event == 'UIEnter' then
+      vim.g.ui_entered = true
+    end
+
+    if file ~= '' and buftype ~= 'nofile' and vim.g.ui_entered then
+      vim.api.nvim_exec_autocmds('User', { pattern = 'FilePost', modeline = false })
+      vim.api.nvim_del_augroup_by_name 'NvFilePost'
+
+      vim.schedule(function()
+        vim.api.nvim_exec_autocmds('FileType', {})
+
+        if vim.g.editorconfig then
+          require('editorconfig').config(args.buf)
+        end
+      end)
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
