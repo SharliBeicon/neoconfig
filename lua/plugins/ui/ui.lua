27e9deda832a2b36df487f624a9f4b4aeaef2391
Charlie Bacon <ct@databeacon.aero>
2025-04-04 11:19:28 +0200

    showkeys

--- lua/plugins/ui/ui.lua
+++ lua/plugins/ui/ui.lua
@@ -1,4 +1,5 @@
 return {
+  { 'nvzone/showkeys', cmd = 'ShowkeysToggle' },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
